d71cecc5c56e6df42fa33c87450cb92bb441301f
Slavko Anakievski <anakievskislavko@gmail.com>
2023-09-01 22:53:55 +0200

    Find nearby places of user's current location

--- lib/src/ui-shared/constants/constants.dart
+++ lib/src/ui-shared/constants/constants.dart
@@ -1,1 +1,2 @@
 const String FIRESTORE_USER_COLLECTION = "users";
+const String API_KEY = "AIzaSyB2oJdloeko4kJYSlLY7JFMuZh2RMcoFfg";
--- lib/src/ui/page/map_page.dart
+++ lib/src/ui/page/map_page.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:travel_point/src/model/nearby_places_response.dart';
+import 'package:travel_point/src/ui-shared/constants/constants.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -19,6 +23,9 @@ class _MapPageState extends State<MapPage> {
 
   late GoogleMapController _googleMapController;
 
+  String radius = "1000";
+  NearbyPlacesResponse nearbyPlacesResponse = NearbyPlacesResponse();
+
   @override
   void dispose() {
     _googleMapController.dispose();
@@ -28,33 +35,48 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GoogleMap(
-          myLocationButtonEnabled: false,
-          zoomControlsEnabled: false,
-          initialCameraPosition: _initialCameraPosition,
-          mapType: MapType.normal,
-          markers: markers,
-          onMapCreated: (controller) => _googleMapController = controller,
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          backgroundColor: Theme.of(context).primaryColor,
-          foregroundColor: Colors.white,
-          onPressed: () async {
-            Position position = await _determinePosition();
-            _googleMapController.animateCamera(CameraUpdate.newCameraPosition(
-                CameraPosition(
-                    target: LatLng(position.latitude, position.longitude),
-                    zoom: 14)));
-            markers.clear();
-            markers.add(Marker(
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        initialCameraPosition: _initialCameraPosition,
+        mapType: MapType.normal,
+        markers: markers,
+        onMapCreated: (controller) => _googleMapController = controller,
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton.extended(
+            backgroundColor: Theme.of(context).primaryColor,
+            foregroundColor: Colors.white,
+            onPressed: () async {
+              Position position = await _determinePosition();
+              _positionCamera(position);
+              markers.clear();
+              markers.add(Marker(
                 markerId: const MarkerId('currentLocation'),
-                position: LatLng(position.latitude, position.longitude)));
+                position: LatLng(position.latitude, position.longitude),
+                infoWindow: const InfoWindow(title: "This is your location!"),
+              ));
 
-            setState(() {});
-          },
-          label: const Text("Current location"),
-          icon: const Icon(Icons.location_history),
-        ));
+              setState(() {});
+            },
+            label: const Text("Current location"),
+            icon: const Icon(Icons.location_history),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton.extended(
+            backgroundColor: Theme.of(context).primaryColor,
+            foregroundColor: Colors.white,
+            onPressed: () {
+              getNearbyPlaces();
+            },
+            label: const Text("Get Nearby Places"),
+            icon: const Icon(Icons.place),
+          ),
+        ],
+      ),
+    );
   }
 
   Future<Position> _determinePosition() async {
@@ -82,5 +104,53 @@ class _MapPageState extends State<MapPage> {
 
     Position position = await Geolocator.getCurrentPosition();
     return position;
+  }
+
+  void getNearbyPlaces() async {
+    Position position = await _determinePosition();
+    var url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.latitude},${position.longitude}&radius=$radius&key=$API_KEY');
+
+    var response = await http.post(url);
+    nearbyPlacesResponse =
+        NearbyPlacesResponse.fromJson(jsonDecode(response.body));
+
+    _drawMarkers();
+    _positionCamera(position);
+    setState(() {});
+  }
+
+  void _drawMarkers() {
+    markers.clear();
+
+    if (nearbyPlacesResponse.results != null) {
+      for (Results result in nearbyPlacesResponse.results!) {
+        if (result.geometry != null &&
+            result.geometry!.location != null &&
+            result.name != null) {
+          markers.add(
+            Marker(
+              markerId: MarkerId(result.placeId ?? ""),
+              position: LatLng(
+                result.geometry!.location!.lat ?? 0.0,
+                result.geometry!.location!.lng ?? 0.0,
+              ),
+              infoWindow: InfoWindow(title: result.name ?? ""),
+            ),
+          );
+        }
+      }
+    }
+  }
+
+  void _positionCamera(Position position) {
+    _googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 18,
+        ),
+      ),
+    );
   }
 }
