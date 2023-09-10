d44ab9255f8229c26c0352a1f719c29a52d01b7f
Dushko Avramoski <Dushko.Avramoski@netcetera.com>
2023-09-10 17:42:00 +0200

    nearby places initial

--- lib/src/features/map/presentation/views/map_page.dart
+++ lib/src/features/map/presentation/views/map_page.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:travel_point/core/constants/constants.dart';
+import 'package:http/http.dart' as http;
 import 'package:travel_point/core/widgets/error_snackbar.dart';
 import 'package:travel_point/src/features/map/presentation/bloc/map_bloc.dart';
 import 'package:travel_point/src/features/map/presentation/bloc/map_event.dart';
@@ -26,7 +29,7 @@ class _MapPageState extends State<MapPage> {
   late GoogleMapController _googleMapController;
 
   String radius = "10000";
-  NearbyPlacesResponse nearbyPlacesResponse = NearbyPlacesResponse();
+  String type = "hair_care";
 
   @override
   void dispose() {
@@ -34,14 +37,7 @@ class _MapPageState extends State<MapPage> {
     super.dispose();
   }
 
-  _drawMarkers(Position position) {
-    markers.clear();
-    markers.add(Marker(
-      markerId: const MarkerId('currentLocation'),
-      position: LatLng(position.latitude, position.longitude),
-      infoWindow: const InfoWindow(title: "This is your location!"),
-    ));
-  }
+  _drawMarker(Position position) {}
 
   _setInitialCameraPosition(Position position) {
     _googleMapController.animateCamera(
@@ -65,10 +61,75 @@ class _MapPageState extends State<MapPage> {
       if (newState is ResultMapState) {
         final position = newState.position;
         _setInitialCameraPosition(position);
-        _drawMarkers(position);
+        _drawMarker(position);
         subscription?.cancel();
       }
     });
+  }
+
+  void getNearbyPlaces() async {
+    final mapBloc = BlocProvider.of<MapBloc>(context);
+    const mapEvent = GetCurrentLocationEvent();
+    mapBloc.add(mapEvent);
+
+    StreamSubscription<MapState>? subscription;
+
+    subscription = mapBloc.stream.listen((newState) async {
+      if (newState is ResultMapState) {
+        final position = newState.position;
+
+        var url = Uri.parse(
+            'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.latitude},${position.longitude}&radius=$radius&type=$type&key=$API_KEY');
+
+        var response = await http.post(url);
+
+        NearbyPlacesResponse nearbyPlacesResponse =
+            NearbyPlacesResponse.fromJson(jsonDecode(response.body));
+
+        print(nearbyPlacesResponse.results?.length);
+
+        _drawMarkers(nearbyPlacesResponse, position);
+        // _drawMarker(position);
+        _setInitialCameraPosition(position);
+        subscription?.cancel();
+
+
+      }
+    });
+  }
+
+  void _drawMarkers(
+      NearbyPlacesResponse nearbyPlacesResponse, Position position) {
+    if (nearbyPlacesResponse.results != null) {
+      Set<Marker> newMarkers = {};
+
+      newMarkers.add(Marker(
+        markerId: const MarkerId('currentLocation'),
+        position: LatLng(position.latitude, position.longitude),
+        infoWindow: const InfoWindow(title: "This is your location!"),
+      ));
+
+      for (Results result in nearbyPlacesResponse.results!) {
+        if (result.geometry != null &&
+            result.geometry!.location != null &&
+            result.name != null) {
+          newMarkers.add(
+            Marker(
+              markerId: MarkerId(result.placeId ?? ''),
+              position: LatLng(
+                result.geometry!.location!.lat ?? 0.0,
+                result.geometry!.location!.lng ?? 0.0,
+              ),
+              infoWindow: InfoWindow(title: result.name ?? ""),
+            ),
+          );
+        }
+      }
+
+      setState(() {
+        markers = newMarkers;
+      });
+    }
   }
 
   Widget defaultScreen(MapState state) {
@@ -79,32 +140,32 @@ class _MapPageState extends State<MapPage> {
         initialCameraPosition: _initialCameraPosition,
         mapType: MapType.normal,
         markers: markers,
-        onMapCreated: (controller) => _googleMapController = controller,
+        onMapCreated: (controller) {
+          _googleMapController = controller;
+        },
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
+          // FloatingActionButton.extended(
+          //   backgroundColor: Theme.of(context).primaryColor,
+          //   foregroundColor: Colors.white,
+          //   onPressed: () async {
+          //     await _handleCurrentLocation(state);
+          //   },
+          //   label: const Text("Current location"),
+          //   icon: const Icon(Icons.location_history),
+          // ),
+          // const SizedBox(height: 16),
           FloatingActionButton.extended(
             backgroundColor: Theme.of(context).primaryColor,
             foregroundColor: Colors.white,
-            onPressed: () async {
-              await _handleCurrentLocation(state);
+            onPressed: () {
+              getNearbyPlaces();
             },
-            label: const Text("Current location"),
-            icon: const Icon(Icons.location_history),
+            label: const Text("Get Nearby Places"),
+            icon: const Icon(Icons.place),
           ),
-          const SizedBox(height: 16),
-          // FloatingActionButton.extended(
-          //   backgroundColor: Theme
-          //       .of(context)
-          //       .primaryColor,
-          //   foregroundColor: Colors.white,
-          //   onPressed: () {
-          //     getNearbyPlaces();
-          //   },
-          //   label: const Text("Get Nearby Places"),
-          //   icon: const Icon(Icons.place),
-          // ),
         ],
       ),
     );
@@ -140,44 +201,6 @@ class _MapPageState extends State<MapPage> {
       );
     });
 
-    //
-    // void getNearbyPlaces() async {
-    //   Position position = await _determinePosition();
-    //   var url = Uri.parse(
-    //       'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position
-    //           .latitude},${position.longitude}&radius=$radius&key=$API_KEY');
-    //
-    //   var response = await http.post(url);
-    //   nearbyPlacesResponse =
-    //       NearbyPlacesResponse.fromJson(jsonDecode(response.body));
-    //
-    //   _drawMarkers();
-    //   _positionCamera(position);
-    //   setState(() {});
-    // }
-    //
-    // void _drawMarkers() {
-    //   markers.clear();
-    //
-    //   if (nearbyPlacesResponse.results != null) {
-    //     for (Results result in nearbyPlacesResponse.results!) {
-    //       if (result.geometry != null &&
-    //           result.geometry!.location != null &&
-    //           result.name != null) {
-    //         markers.add(
-    //           Marker(
-    //             markerId: MarkerId(result.placeId ?? ""),
-    //             position: LatLng(
-    //               result.geometry!.location!.lat ?? 0.0,
-    //               result.geometry!.location!.lng ?? 0.0,
-    //             ),
-    //             infoWindow: InfoWindow(title: result.name ?? ""),
-    //           ),
-    //         );
-    //       }
-    //     }
-    //   }
-    // }
     //
     // void _positionCamera(Position position) {
     //   _googleMapController.animateCamera(
